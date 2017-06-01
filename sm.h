af1bcd56c4e612bbd9f02810b4c098e0a10edbce
Thiago Borges de Oliveira <thborges@gmail.com>
2017-06-01 18:31:34 -0300

    Missing file.

---
+++ sm.h
@@ -0,0 +1,10 @@
+#ifndef SM_H
+#define SM_H
+
+double get_sm_objective(dataset_histogram *hr, optimization_data_s *opt_data, int opt_atu,
+	double f, double g, int servers, double multiplier, double *x0_ub, double *x0_lb, double *fmkspan, double *fcomm);
+
+
+
+#endif
+
